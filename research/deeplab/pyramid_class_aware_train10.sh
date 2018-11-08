80e393d47ce48344272feeffe50a07e9c54b505a
long <908865817@qq.com>
2018-11-08 14:56:46 +0800

    update py 9 10

--- research/deeplab/pyramid_class_aware_train10.sh
+++ research/deeplab/pyramid_class_aware_train10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0,1
-python pyramid_class_aware_train9.py\
+python pyramid_class_aware_train10.py\
     --logtostderr \
     --num_clones=2 \
     --train_split="train_aug"\
@@ -19,7 +19,7 @@ python pyramid_class_aware_train9.py\
     --base_learning_rate=0.007\
     --dataset="pascal_voc_seg"\
     --tf_initial_checkpoint="datasets/pascal_voc_seg/init_models/xception/model.ckpt"\
-    --train_logdir="datasets/pascal_voc_seg/exp/pyramid_class_aware_train9_on_trainaug_set/train"\
+    --train_logdir="datasets/pascal_voc_seg/exp/pyramid_class_aware_train10_on_trainaug_set/train"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"
 
 #python pyramid_class_aware_eval6.py\
@@ -38,7 +38,7 @@ python pyramid_class_aware_train9.py\
 #    --eval_logdir="datasets/pascal_voc_seg/exp/pyramid_class_aware_train6_on_trainaug_set/eval"\
 #    --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
 #    --max_number_of_evaluations=1449
-python pyramid_class_aware_train9.py\
+python pyramid_class_aware_train10.py\
     --logtostderr \
     --num_clones=2 \
     --train_split="train_aug"\
@@ -56,12 +56,12 @@ python pyramid_class_aware_train9.py\
     --fine_tune_batch_norm=false\
     --base_learning_rate=0.001\
     --dataset="pascal_voc_seg"\
-    --tf_initial_checkpoint="datasets/pascal_voc_seg/exp/pyramid_class_aware_train9_on_trainaug_set/train/model.ckpt-30000"\
-    --train_logdir="datasets/pascal_voc_seg/exp/pyramid_class_aware_train9_on_trainaug_set/train_finetune"\
+    --tf_initial_checkpoint="datasets/pascal_voc_seg/exp/pyramid_class_aware_train10_on_trainaug_set/train/model.ckpt-30000"\
+    --train_logdir="datasets/pascal_voc_seg/exp/pyramid_class_aware_train10_on_trainaug_set/train_finetune"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"
 
 
-python pyramid_class_aware_eval9.py\
+python pyramid_class_aware_eval10.py\
     --logtostderr\
     --eval_split="val"\
     --model_variant="xception_65"\
@@ -73,7 +73,7 @@ python pyramid_class_aware_eval9.py\
     --eval_crop_size=513\
     --eval_crop_size=513\
     --dataset="pascal_voc_seg"\
-    --checkpoint_dir="datasets/pascal_voc_seg/exp/pyramid_class_aware_train9_on_trainaug_set/train_finetune"\
-    --eval_logdir="datasets/pascal_voc_seg/exp/pyramid_class_aware_train9_on_trainaug_set/eval_finetune"\
+    --checkpoint_dir="datasets/pascal_voc_seg/exp/pyramid_class_aware_train10_on_trainaug_set/train_finetune"\
+    --eval_logdir="datasets/pascal_voc_seg/exp/pyramid_class_aware_train10_on_trainaug_set/eval_finetune"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
     --max_number_of_evaluations=1449
