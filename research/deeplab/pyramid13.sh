e94c2814d1a42d9f6c4008342f66a2a1ecb2742f
long <908865817@qq.com>
2018-11-14 19:43:33 +0800

    py16

--- research/deeplab/pyramid13.sh
+++ research/deeplab/pyramid13.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=6,7 sh pyramid_class_aware_train13.sh 2>&1 | tee pyramid_class_aware_train13.log
+sh pyramid_class_aware_train13.sh 2>&1 | tee pyramid_class_aware_train13.log
 
-CUDA_VISIBLE_DEVICES=6,7 sh pyramid_class_aware_train13_res50.sh 2>&1 | tee pyramid_class_aware_train13_res50.log
+sh pyramid_class_aware_train13_res50.sh 2>&1 | tee pyramid_class_aware_train13_res50.log
 
-CUDA_VISIBLE_DEVICES=6,7 sh pyramid_class_aware_train13_res101.sh 2>&1 | tee pyramid_class_aware_train13_res101.log
+sh pyramid_class_aware_train13_res101.sh 2>&1 | tee pyramid_class_aware_train13_res101.log
--- research/deeplab/v3_v3plus_train.sh
+++ research/deeplab/v3_v3plus_train.sh
@@ -7,3 +7,7 @@ sh v3train_res50.sh 2>&1 | tee v3train_res50.log
 sh v3train_res101.sh 2>&1 | tee v3train_res101.log
 
 sh v3plustrain.sh 2>&1 | tee v3plustrain.log
+
+sh pyramid_class_aware_train17_res50.sh 2>&1 | tee pyramid_class_aware_train17_res50.log
+
+sh pyramid_class_aware_train16_res50.sh 2>&1 | tee pyramid_class_aware_train16_res50.log
