a5826459c8c7d227b4b463d32f3e928a2533eed4
long <908865817@qq.com>
2018-11-14 19:12:22 +0800

    LR

--- research/deeplab/v3_v3plus_train.sh
+++ research/deeplab/v3_v3plus_train.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=6,7 sh v3train.sh 2>&1 | tee v3train.log
+sh v3train.sh 2>&1 | tee v3train.log
 
-CUDA_VISIBLE_DEVICES=6,7 sh v3train_res50.sh 2>&1 | tee v3train_res50.log
+sh v3train_res50.sh 2>&1 | tee v3train_res50.log
 
-CUDA_VISIBLE_DEVICES=6,7 sh v3train_res101.sh 2>&1 | tee v3train_res101.log
+sh v3train_res101.sh 2>&1 | tee v3train_res101.log
 
-CUDA_VISIBLE_DEVICES=6,7 sh v3plustrain.sh 2>&1 | tee v3plustrain.log
+sh v3plustrain.sh 2>&1 | tee v3plustrain.log
