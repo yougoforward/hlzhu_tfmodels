f353888c2b99ce1783c767552a33ac6b2c0c4d33
long <908865817@qq.com>
2018-11-16 19:13:01 +0800

    2021

--- research/deeplab/pyramid_class_aware_model21.py
+++ research/deeplab/pyramid_class_aware_model21.py
@@ -1313,7 +1313,7 @@ def get_class_aware_attention_branch_logits1(features,
                             is_training=is_training,
                             fine_tune_batch_norm=aspp_with_batch_norm)
   with tf.variable_scope("aspp2", "aspp2"):
-      features_aspp2 = ASPP(features,
+      features_aspp2 = ASPP2(features,
                             model_options,
                             weight_decay=weight_decay,
                             reuse=reuse,
@@ -1457,7 +1457,7 @@ def get_class_aware_attention_branch_logits(features,
 
   f2 = slim.conv2d(
       features,
-      256,
+      128,
       kernel_size=1,
       rate=1,
       activation_fn=tf.nn.relu,
@@ -1831,3 +1831,109 @@ def class_aware_extract_features(images,
       is_training=is_training,
       fine_tune_batch_norm=fine_tune_batch_norm)
   return features, end_points
+
+def ASPP2(features,
+                     model_options,
+                     weight_decay=0.0001,
+                     reuse=None,
+                     is_training=False,
+                     fine_tune_batch_norm=False):
+    """
+
+    :param features:
+    :param model_options:
+    :param weight_decay:
+    :param reuse:
+    :param is_training:
+    :param fine_tune_batch_norm:
+    :return:
+    """
+
+    batch_norm_params = {
+        'is_training': is_training and fine_tune_batch_norm,
+        'decay': 0.9997,
+        'epsilon': 1e-5,
+        'scale': True,
+    }
+
+    with slim.arg_scope(
+            [slim.conv2d, slim.separable_conv2d],
+            weights_regularizer=slim.l2_regularizer(weight_decay),
+            activation_fn=tf.nn.relu,
+            normalizer_fn=slim.batch_norm,
+            padding='SAME',
+            stride=1,
+            reuse=reuse):
+        with slim.arg_scope([slim.batch_norm], **batch_norm_params):
+            features=slim.conv2d(features, 512, 1,
+                        scope=ASPP_SCOPE + '_reduce')
+            depth = 256
+            branch_logits = []
+
+            if model_options.add_image_level_feature:
+                if model_options.crop_size is not None:
+                    image_pooling_crop_size = model_options.image_pooling_crop_size
+                    # If image_pooling_crop_size is not specified, use crop_size.
+                    if image_pooling_crop_size is None:
+                        image_pooling_crop_size = model_options.crop_size
+                    pool_height = scale_dimension(image_pooling_crop_size[0],
+                                                  1. / model_options.output_stride)
+                    pool_width = scale_dimension(image_pooling_crop_size[1],
+                                                 1. / model_options.output_stride)
+                    image_feature = slim.avg_pool2d(
+                        features, [pool_height, pool_width], [1, 1], padding='VALID')
+                    resize_height = scale_dimension(model_options.crop_size[0],
+                                                    1. / model_options.output_stride)
+                    resize_width = scale_dimension(model_options.crop_size[1],
+                                                   1. / model_options.output_stride)
+                else:
+                    # If crop_size is None, we simply do global pooling.
+                    pool_height = tf.shape(features)[1]
+                    pool_width = tf.shape(features)[2]
+                    image_feature = tf.reduce_mean(features, axis=[1, 2])[:, tf.newaxis,
+                                    tf.newaxis]
+                    resize_height = pool_height
+                    resize_width = pool_width
+                image_feature = slim.conv2d(
+                    image_feature, depth, 1, scope=IMAGE_POOLING_SCOPE)
+                image_feature = tf.image.resize_bilinear(
+                    image_feature, [resize_height, resize_width], align_corners=True)
+                # Set shape for resize_height/resize_width if they are not Tensor.
+                if isinstance(resize_height, tf.Tensor):
+                    resize_height = None
+                if isinstance(resize_width, tf.Tensor):
+                    resize_width = None
+                image_feature.set_shape([None, resize_height, resize_width, depth])
+                branch_logits.append(image_feature)
+
+            # Employ a 1x1 convolution.
+            branch_logits.append(slim.conv2d(features, depth, 1,
+                                             scope=ASPP_SCOPE + str(0)))
+
+            if model_options.atrous_rates:
+                # Employ 3x3 convolutions with different atrous rates.
+                for i, rate in enumerate(model_options.atrous_rates, 1):
+                    scope = ASPP_SCOPE + str(i)
+                    if model_options.aspp_with_separable_conv:
+                        aspp_features = split_separable_conv2d(
+                            features,
+                            filters=depth,
+                            rate=rate,
+                            weight_decay=weight_decay,
+                            scope=scope)
+                    else:
+                        aspp_features = slim.conv2d(
+                            features, depth, 3, rate=rate, scope=scope)
+                    branch_logits.append(aspp_features)
+
+            # Merge branch logits.
+            concat_logits = tf.concat(branch_logits, 3)
+            concat_logits = slim.conv2d(
+                concat_logits, depth, 1, scope=CONCAT_PROJECTION_SCOPE)
+            concat_logits = slim.dropout(
+                concat_logits,
+                keep_prob=0.9,
+                is_training=is_training,
+                scope=CONCAT_PROJECTION_SCOPE + '_dropout')
+
+            return concat_logits
