db8716a946a904806edf25ee6bec06ee3ee91230
long <908865817@qq.com>
2018-11-16 14:57:28 +0800

    2021

---
+++ research/deeplab/pyramid20.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#sh class_aware_train15.sh 2>&1 | tee class_aware_train15.log
+#
+#sh class_aware_train15_res101.sh 2>&1 | tee class_aware_train16_res101.log
+
+#sh pyramid_class_aware_train17_res50.sh 2>&1 | tee pyramid_class_aware_train17_res50.log
+#
+#sh pyramid_class_aware_train16_res50.sh 2>&1 | tee pyramid_class_aware_train16_res50.log
+#
+#sh pyramid_class_aware_train16.sh 2>&1 | tee pyramid_class_aware_train16.log
+
+
+#sh pyramid_class_aware_train16_res101.sh 2>&1 | tee pyramid_class_aware_train16_res101.log
+
+sh pyramid_class_aware_train22_res50.sh 2>&1 | tee pyramid_class_aware_train22_res50.log
+
+sh pyramid_class_aware_train20_res50.sh 2>&1 | tee pyramid_class_aware_train20_res50.log
+
+sh pyramid_class_aware_train20.sh 2>&1 | tee pyramid_class_aware_train20.log
+
+
+
