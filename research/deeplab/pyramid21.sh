d98dcc451e757630771d075cefeb5b0f89bc08e2
long <908865817@qq.com>
2018-11-17 10:12:33 +0800

    21

--- research/deeplab/pyramid21.sh
+++ research/deeplab/pyramid21.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sh pyramid_class_aware_train23_res50.sh 2>&1 | tee pyramid_class_aware_train23_res50.log
+sh pyramid_class_aware_train23.sh 2>&1 | tee pyramid_class_aware_train23_res50.log
 
 sh pyramid_class_aware_train21_res50.sh 2>&1 | tee pyramid_class_aware_train21_res50.log
 
-sh pyramid_class_aware_train21.sh 2>&1 | tee pyramid_class_aware_train21.log
+#sh pyramid_class_aware_train21.sh 2>&1 | tee pyramid_class_aware_train21.log
