e7540e20cabb847a558aa114785682489bf341d9
long <908865817@qq.com>
2018-11-28 19:16:28 +0800

    dual

---
+++ research/deeplab/dual_pyramid.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+sh merge2aspp_train.sh 2>&1 | tee merge2aspp_train.log
+sh dual_pyramid_train.sh 2>&1 | tee dual_pyramid_train.log
+
+#sh dual_pyramid_train_with_boundary.sh 2>&1 | tee dual_pyramid_train_with_boundary.log
+
