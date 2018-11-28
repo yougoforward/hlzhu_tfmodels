fabd73fafe4dd24f58fa6694202cad4ef240b8ea
long <908865817@qq.com>
2018-11-28 10:39:59 +0800

    cam15

--- research/deeplab/cam15.sh
+++ research/deeplab/cam15.sh
@@ -3,4 +3,4 @@ sh class_aware_train15_add2.sh 2>&1 | tee class_aware_train15_add2_res50.log
 
 #sh class_aware_train15_res50.sh 2>&1 | tee class_aware_train15_res50.log
 
-sh class_aware_train15_loss2.sh 2>&1 | tee class_aware_train15_loss2_res50.log
+#sh class_aware_train15_loss2.sh 2>&1 | tee class_aware_train15_loss2_res50.log
