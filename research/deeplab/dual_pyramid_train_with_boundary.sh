255c1c8e4361f56a2c7e19589b277c58917642d0
long <908865817@qq.com>
2018-11-29 10:37:19 +0800

    boundary

--- research/deeplab/dual_pyramid_train_with_boundary.sh
+++ research/deeplab/dual_pyramid_train_with_boundary.sh
@@ -20,7 +20,7 @@ python dual_pyramid_train_with_boundary.py\
     --train_crop_size=513\
     --train_batch_size=16\
     --decoder_output_stride=4\
-    --training_number_of_steps=30000\
+    --training_number_of_steps=40000\
     --fine_tune_batch_norm=true\
     --base_learning_rate=0.007\
     --weight_decay=0.0001\
@@ -55,53 +55,53 @@ python dual_pyramid_eval_with_boundary.py\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
     --max_number_of_evaluations=1
 
-python dual_pyramid_train_with_boundary.py\
-    --logtostderr\
-    --num_clones=2 \
-    --train_split="train_aug"\
-    --model_variant="resnet_v1_50_beta"\
-    --atrous_rates=6\
-    --atrous_rates=12\
-    --atrous_rates=18\
-    --output_stride=16\
-    --decoder_output_stride=4\
-    --train_crop_size=513\
-    --train_crop_size=513\
-    --train_batch_size=16\
-    --multi_grid=1\
-    --multi_grid=2\
-    --multi_grid=4\
-    --training_number_of_steps=30000\
-    --fine_tune_batch_norm=false\
-    --base_learning_rate=0.001\
-    --weight_decay=0.0001\
-    --aspp_with_batch_norm=true\
-    --aspp_with_separable_conv=false\
-    --decoder_use_separable_conv=false\
-    --dataset="pascal_voc_seg"\
-    --tf_initial_checkpoint="datasets/pascal_voc_seg/exp/dual_pyramid_train_with_boundary_res50_on_trainaug_set/train/model.ckpt-30000"\
-    --train_logdir="datasets/pascal_voc_seg/exp/dual_pyramid_train_with_boundary_res50_on_trainaug_set/train_finetune"\
-    --dataset_dir="datasets/pascal_voc_seg/tfrecord"
-
-python dual_pyramid_eval_with_boundary.py\
-    --logtostderr\
-    --eval_split="val"\
-    --model_variant="resnet_v1_50_beta"\
-    --atrous_rates=6\
-    --atrous_rates=12\
-    --atrous_rates=18\
-    --output_stride=16\
-    --multi_grid=1\
-    --multi_grid=2\
-    --multi_grid=4\
-    --decoder_output_stride=4\
-    --eval_crop_size=513\
-    --eval_crop_size=513\
-    --aspp_with_batch_norm=true\
-    --aspp_with_separable_conv=false\
-    --decoder_use_separable_conv=false\
-    --dataset="pascal_voc_seg"\
-    --checkpoint_dir="datasets/pascal_voc_seg/exp/dual_pyramid_train_with_boundary_res50_on_trainaug_set/train_finetune"\
-    --eval_logdir="datasets/pascal_voc_seg/exp/dual_pyramid_train_with_boundary_res50_on_trainaug_set/eval_finetune"\
-    --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
-    --max_number_of_evaluations=1
+#python dual_pyramid_train_with_boundary.py\
+#    --logtostderr\
+#    --num_clones=2 \
+#    --train_split="train_aug"\
+#    --model_variant="resnet_v1_50_beta"\
+#    --atrous_rates=6\
+#    --atrous_rates=12\
+#    --atrous_rates=18\
+#    --output_stride=16\
+#    --decoder_output_stride=4\
+#    --train_crop_size=513\
+#    --train_crop_size=513\
+#    --train_batch_size=16\
+#    --multi_grid=1\
+#    --multi_grid=2\
+#    --multi_grid=4\
+#    --training_number_of_steps=30000\
+#    --fine_tune_batch_norm=false\
+#    --base_learning_rate=0.001\
+#    --weight_decay=0.0001\
+#    --aspp_with_batch_norm=true\
+#    --aspp_with_separable_conv=false\
+#    --decoder_use_separable_conv=false\
+#    --dataset="pascal_voc_seg"\
+#    --tf_initial_checkpoint="datasets/pascal_voc_seg/exp/dual_pyramid_train_with_boundary_res50_on_trainaug_set/train/model.ckpt-30000"\
+#    --train_logdir="datasets/pascal_voc_seg/exp/dual_pyramid_train_with_boundary_res50_on_trainaug_set/train_finetune"\
+#    --dataset_dir="datasets/pascal_voc_seg/tfrecord"
+#
+#python dual_pyramid_eval_with_boundary.py\
+#    --logtostderr\
+#    --eval_split="val"\
+#    --model_variant="resnet_v1_50_beta"\
+#    --atrous_rates=6\
+#    --atrous_rates=12\
+#    --atrous_rates=18\
+#    --output_stride=16\
+#    --multi_grid=1\
+#    --multi_grid=2\
+#    --multi_grid=4\
+#    --decoder_output_stride=4\
+#    --eval_crop_size=513\
+#    --eval_crop_size=513\
+#    --aspp_with_batch_norm=true\
+#    --aspp_with_separable_conv=false\
+#    --decoder_use_separable_conv=false\
+#    --dataset="pascal_voc_seg"\
+#    --checkpoint_dir="datasets/pascal_voc_seg/exp/dual_pyramid_train_with_boundary_res50_on_trainaug_set/train_finetune"\
+#    --eval_logdir="datasets/pascal_voc_seg/exp/dual_pyramid_train_with_boundary_res50_on_trainaug_set/eval_finetune"\
+#    --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
+#    --max_number_of_evaluations=1
