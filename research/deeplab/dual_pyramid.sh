b9c5001ffe0e9ad35f4ba1e30be4a509c0637daa
long <908865817@qq.com>
2018-11-29 11:19:23 +0800

    boundary

--- research/deeplab/dual_pyramid.sh
+++ research/deeplab/dual_pyramid.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sh merge2aspp_train.sh 2>&1 | tee merge2aspp_train.log
+#sh merge2aspp_train.sh 2>&1 | tee merge2aspp_train.log
 sh dual_pyramid_train.sh 2>&1 | tee dual_pyramid_train.log
 
 #sh dual_pyramid_train_with_boundary.sh 2>&1 | tee dual_pyramid_train_with_boundary.log
