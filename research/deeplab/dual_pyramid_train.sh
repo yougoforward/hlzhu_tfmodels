fd2f526a92cea5fbe6b0ad3350403da9f6985150
long <908865817@qq.com>
2018-12-20 16:16:26 +0800

    merge

--- research/deeplab/dual_pyramid_train.sh
+++ research/deeplab/dual_pyramid_train.sh
@@ -104,4 +104,4 @@ python dual_pyramid_eval.py\
     --checkpoint_dir="datasets/pascal_voc_seg/exp/dual_pyramid_train_res50_on_trainaug_set/train_finetune"\
     --eval_logdir="datasets/pascal_voc_seg/exp/dual_pyramid_train_res50_on_trainaug_set/eval_finetune"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
-    --max_number_of_evaluations=1#!/usr/bin/env bash
+    --max_number_of_evaluations=1
