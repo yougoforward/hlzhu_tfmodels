05890d17a72f22c395ee5465f0318b695469e65f
long <908865817@qq.com>
2018-12-29 14:39:52 +0800

    rrb gau

--- research/deeplab/dual_pyramid.sh
+++ research/deeplab/dual_pyramid.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #sh merge2aspp_train.sh 2>&1 | tee merge2aspp_train.log
-sh dual_pyramid_train.sh 2>&1 | tee dual_pyramid_train8.log
+sh dual_pyramid_train.sh 2>&1 | tee dual_pyramid_train9.log
 
-sh dual_pyramid_train_with_boundary.sh 2>&1 | tee dual_pyramid_train_with_boundary8.log
+sh dual_pyramid_train_with_boundary.sh 2>&1 | tee dual_pyramid_train_with_boundary9.log
 
