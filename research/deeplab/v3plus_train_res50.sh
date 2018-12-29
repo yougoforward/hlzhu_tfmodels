82ad34c3fb7fff809b18595a123df99a8697c8d2
long <908865817@qq.com>
2018-12-29 15:23:39 +0800

    v3p50

--- research/deeplab/v3plus_train_res50.sh
+++ research/deeplab/v3plus_train_res50.sh
@@ -29,7 +29,7 @@ python train.py\
     --decoder_use_separable_conv=false\
     --dataset="pascal_voc_seg"\
     --tf_initial_checkpoint="datasets/pascal_voc_seg/init_models/resnet_v1_50/model.ckpt"\
-    --train_logdir="datasets/pascal_voc_seg/exp/v3_train_res50_on_trainaug_set/train"\
+    --train_logdir="datasets/pascal_voc_seg/exp/v3plus_train_res50_on_trainaug_set/train"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"
 
 python eval.py\
@@ -50,8 +50,8 @@ python eval.py\
     --aspp_with_separable_conv=false\
     --decoder_use_separable_conv=false\
     --dataset="pascal_voc_seg"\
-    --checkpoint_dir="datasets/pascal_voc_seg/exp/v3_train_res50_on_trainaug_set/train"\
-    --eval_logdir="datasets/pascal_voc_seg/exp/v3_train_res50_on_trainaug_set/eval"\
+    --checkpoint_dir="datasets/pascal_voc_seg/exp/v3plus_train_res50_on_trainaug_set/train"\
+    --eval_logdir="datasets/pascal_voc_seg/exp/v3plus_train_res50_on_trainaug_set/eval"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
     --max_number_of_evaluations=1
 
@@ -79,8 +79,8 @@ python train.py\
     --aspp_with_separable_conv=false\
     --decoder_use_separable_conv=false\
     --dataset="pascal_voc_seg"\
-    --tf_initial_checkpoint="datasets/pascal_voc_seg/exp/v3_train_res50_on_trainaug_set/train/model.ckpt-30000"\
-    --train_logdir="datasets/pascal_voc_seg/exp/v3_train_res50_on_trainaug_set/train_finetune"\
+    --tf_initial_checkpoint="datasets/pascal_voc_seg/exp/v3plus_train_res50_on_trainaug_set/train/model.ckpt-30000"\
+    --train_logdir="datasets/pascal_voc_seg/exp/v3plus_train_res50_on_trainaug_set/train_finetune"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"
 
 python eval.py\
@@ -101,7 +101,7 @@ python eval.py\
     --aspp_with_separable_conv=false\
     --decoder_use_separable_conv=false\
     --dataset="pascal_voc_seg"\
-    --checkpoint_dir="datasets/pascal_voc_seg/exp/v3_train_res50_on_trainaug_set/train_finetune"\
-    --eval_logdir="datasets/pascal_voc_seg/exp/v3_train_res50_on_trainaug_set/eval_finetune"\
+    --checkpoint_dir="datasets/pascal_voc_seg/exp/v3plus_train_res50_on_trainaug_set/train_finetune"\
+    --eval_logdir="datasets/pascal_voc_seg/exp/v3plus_train_res50_on_trainaug_set/eval_finetune"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
     --max_number_of_evaluations=1
