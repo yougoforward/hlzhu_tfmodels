2ae501634e6abe8951434d7f34ff792107c3d73b
long <908865817@qq.com>
2018-12-29 23:21:16 +0800

    v3plusCam

--- research/deeplab/dual_pyramid.sh
+++ research/deeplab/dual_pyramid.sh
@@ -4,3 +4,4 @@ sh dual_pyramid_train.sh 2>&1 | tee dual_pyramid_train9.log
 
 sh dual_pyramid_train_with_boundary.sh 2>&1 | tee dual_pyramid_train_with_boundary9.log
 
+sh dual_pyramid_train_v3plusCam.sh 2>&1 | tee dual_pyramid_train_v3plusCam.log
