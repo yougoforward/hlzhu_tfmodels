f25c94f30f39cbca65c1feea5d233d63231a7f3d
long <908865817@qq.com>
2019-01-06 18:51:27 +0800

    skip481x1

--- research/deeplab/nosoftmax1loss_nocam1super.sh
+++ research/deeplab/nosoftmax1loss_nocam1super.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sh class_aware_train15_3_res50.sh 2>&1 |tee class_aware_train15_nosoftmax1loss_res50.log
-sh dual_pyramid_train_with_boundary.sh 2>&1 | tee dual_pyramid_train_with_boundary9.log
+sh dual_pyramid_train_with_boundary.sh 2>&1 | tee dual_pyramid_train_nocam1super.log
