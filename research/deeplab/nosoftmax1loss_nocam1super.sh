6d9b102884d7743206640dbae44748ff9896b337
long <908865817@qq.com>
2019-01-07 19:17:35 +0800

    test

--- research/deeplab/nosoftmax1loss_nocam1super.sh
+++ research/deeplab/nosoftmax1loss_nocam1super.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-sh class_aware_train15_3_res50.sh 2>&1 |tee class_aware_train15_nosoftmax1loss_res50.log
-sh dual_pyramid_train_with_boundary.sh 2>&1 | tee dual_pyramid_train_nocam1super.log
+#sh class_aware_train15_3_res50.sh 2>&1 |tee class_aware_train15_nosoftmax1loss_res50.log
+
+sh class_aware_train15_4_res50.sh 2>&1 |tee class_aware_train15_nosigmoidsoftmax1loss_res50.log
+
+sh class_aware_train15_5_res50.sh 2>&1 |tee class_aware_train15_nosigmoidloss_res50.log
