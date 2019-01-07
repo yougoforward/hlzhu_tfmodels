71d3b288a8ff946206a90f35cf27ef6cc05e7507
long <908865817@qq.com>
2019-01-07 18:48:54 +0800

    cam15x65

--- research/deeplab/cam15.sh
+++ research/deeplab/cam15.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sh class_aware_train15.sh 2>&1 | tee class_aware_train15.log
 
-sh class_aware_train15_res50.sh 2>&1 | tee class_aware_train15_res50.log
+#sh class_aware_train15_res50.sh 2>&1 | tee class_aware_train15_res50.log
 
 #sh class_aware_train15_loss2.sh 2>&1 | tee class_aware_train15_loss2_res50.log
