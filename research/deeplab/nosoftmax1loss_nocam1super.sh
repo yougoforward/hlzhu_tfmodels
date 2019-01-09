2c31c58f872f087b604e888f97fab57c966af329
long <908865817@qq.com>
2019-01-09 09:12:14 +0800

    60000fintune

--- research/deeplab/nosoftmax1loss_nocam1super.sh
+++ research/deeplab/nosoftmax1loss_nocam1super.sh
@@ -3,4 +3,4 @@ sh class_aware_train15_3_res50.sh 2>&1 |tee class_aware_train15_nosoftmax1loss_r
 
 sh class_aware_train15_4_res50.sh 2>&1 |tee class_aware_train15_nosigmoidsoftmax1loss_res50.log
 
-#sh class_aware_train15_5_res50.sh 2>&1 |tee class_aware_train15_nosigmoidloss_res50.log
+sh class_aware_train15_5_res50.sh 2>&1 |tee class_aware_train15_nosigmoidloss_res50.log
