cce523f11d0618f1772e1e04f89dec43b914d39e
long <908865817@qq.com>
2019-01-10 14:41:11 +0800

    dpw

--- research/deeplab/class_aware_train15_nosigmoidloss.py
+++ research/deeplab/class_aware_train15_nosigmoidloss.py
@@ -236,7 +236,7 @@ def _build_deeplab(inputs_queue, outputs_to_num_classes, ignore_label):
   #       samples[common.LABEL],
   #       num_classes,
   #       ignore_label,
-  #       loss_weight=0.0,
+  #       loss_weight=1.0,
   #       upsample_logits=FLAGS.upsample_logits,
   #       scope=output)
   for output, num_classes in six.iteritems(outputs_to_num_classes):
@@ -245,7 +245,7 @@ def _build_deeplab(inputs_queue, outputs_to_num_classes, ignore_label):
         samples[common.LABEL],
         num_classes,
         ignore_label,
-        loss_weight=1.0,
+        loss_weight=0.1,
         upsample_logits=FLAGS.upsample_logits,
         scope=output)
 
--- research/deeplab/dpconcat.sh
+++ research/deeplab/dpconcat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sh dual_pyramid_train.sh 2>&1 | tee dual_pyramid_train_concat256.log
-#sh dual_pyramid_train_x65.sh 2>&1 | tee dual_pyramid_train_x65_concat256.log
+sh dual_pyramid_train_x65.sh 2>&1 | tee dual_pyramid_train_x65_concat256.log
