612a8d1e02df3b1848155ea1d3535a081bdc39ef
long <908865817@qq.com>
2019-01-10 15:12:38 +0800

    dpw

--- research/deeplab/cam15.sh
+++ research/deeplab/cam15.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#sh class_aware_train15.sh 2>&1 | tee class_aware_train15.log
+sh class_aware_train15.sh 2>&1 | tee class_aware_train15.log
 
 sh class_aware_train15_res50.sh 2>&1 | tee class_aware_train15_res50.log
 
