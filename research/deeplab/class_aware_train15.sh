d2313c640824106072ea1c1b3a2f673c83340072
long <908865817@qq.com>
2019-01-10 15:39:25 +0800

    dpw

--- research/deeplab/class_aware_train15.sh
+++ research/deeplab/class_aware_train15.sh
@@ -71,14 +71,65 @@ cd ./deeplab
 #    --train_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/train_finetune2"\
 #    --dataset_dir="datasets/pascal_voc_seg/tfrecord"
 
+#python class_aware_eval15.py\
+#    --logtostderr\
+#    --eval_split="val"\
+#    --model_variant="xception_65"\
+#    --atrous_rates=6\
+#    --atrous_rates=12\
+#    --atrous_rates=18\
+#    --output_stride=16\
+#    --eval_crop_size=513\
+#    --eval_crop_size=513\
+#    --aspp_with_batch_norm=true\
+#    --aspp_with_separable_conv=false\
+#    --decoder_use_separable_conv=false\
+#    --dataset="pascal_voc_seg"\
+#    --checkpoint_dir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/train_finetune2"\
+#    --eval_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/eval_finetune2"\
+#    --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
+#    --max_number_of_evaluations=1\
+#    --eval_scales=0.5\
+#    --eval_scales=0.75\
+#    --eval_scales=1.0\
+#    --eval_scales=1.25\
+#    --eval_scales=1.5\
+#    --eval_scales=1.75\
+#    --add_flipped_images=true
+
+
+#python class_aware_train15.py\
+#    --logtostderr\
+#    --num_clones=2\
+#    --train_split="train_aug"\
+#    --model_variant="xception_65"\
+#    --atrous_rates=6\
+#    --atrous_rates=12\
+#    --atrous_rates=18\
+#    --output_stride=16\
+#    --train_crop_size=513\
+#    --train_crop_size=513\
+#    --train_batch_size=16\
+#    --training_number_of_steps=30000\
+#    --fine_tune_batch_norm=false\
+#    --base_learning_rate=0.001\
+#    --weight_decay=0.00004\
+#    --aspp_with_batch_norm=true\
+#    --aspp_with_separable_conv=false\
+#    --decoder_use_separable_conv=false\
+#    --dataset="pascal_voc_seg"\
+#    --tf_initial_checkpoint="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/train/model.ckpt-60000"\
+#    --train_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/train_finetune2"\
+#    --dataset_dir="datasets/pascal_voc_seg/tfrecord"
+
 python class_aware_eval15.py\
     --logtostderr\
     --eval_split="val"\
     --model_variant="xception_65"\
-    --atrous_rates=6\
     --atrous_rates=12\
-    --atrous_rates=18\
-    --output_stride=16\
+    --atrous_rates=24\
+    --atrous_rates=36\
+    --output_stride=8\
     --eval_crop_size=513\
     --eval_crop_size=513\
     --aspp_with_batch_norm=true\
@@ -88,11 +139,11 @@ python class_aware_eval15.py\
     --checkpoint_dir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/train_finetune2"\
     --eval_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/eval_finetune2"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
-    --max_number_of_evaluations=1\
-    --eval_scales=0.5\
-    --eval_scales=0.75\
-    --eval_scales=1.0\
-    --eval_scales=1.25\
-    --eval_scales=1.5\
-    --eval_scales=1.75\
-    --add_flipped_images=true
+    --max_number_of_evaluations=1
+#    --eval_scales=0.5\
+#    --eval_scales=0.75\
+#    --eval_scales=1.0\
+#    --eval_scales=1.25\
+#    --eval_scales=1.5\
+#    --eval_scales=1.75\
+#    --add_flipped_images=true
