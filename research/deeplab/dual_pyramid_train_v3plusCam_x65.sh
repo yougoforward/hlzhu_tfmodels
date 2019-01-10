6b97809f965863982119465636215c689047fb84
long <908865817@qq.com>
2019-01-10 17:31:11 +0800

    dpw

--- research/deeplab/dual_pyramid_train_v3plusCam_x65.sh
+++ research/deeplab/dual_pyramid_train_v3plusCam_x65.sh
@@ -134,10 +134,10 @@ python dual_pyramid_eval_v3plusCam.py\
     --logtostderr\
     --eval_split="val"\
     --model_variant="xception_65"\
+    --atrous_rates=6\
     --atrous_rates=12\
-    --atrous_rates=24\
-    --atrous_rates=36\
-    --output_stride=8\
+    --atrous_rates=18\
+    --output_stride=16\
     --decoder_output_stride=4\
     --eval_crop_size=513\
     --eval_crop_size=513\
@@ -148,11 +148,11 @@ python dual_pyramid_eval_v3plusCam.py\
     --checkpoint_dir="datasets/pascal_voc_seg/exp/dual_pyramid_train_v3plusCam_xception65_on_trainaug_set/train_finetune11"\
     --eval_logdir="datasets/pascal_voc_seg/exp/dual_pyramid_train_v3plusCam_xception65_on_trainaug_set/eval_finetune11"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
-    --max_number_of_evaluations=1\
-    --eval_scales=0.5\
-    --eval_scales=0.75\
-    --eval_scales=1.0\
-    --eval_scales=1.25\
-    --eval_scales=1.5\
-    --eval_scales=1.75\
-    --add_flipped_images=true
+    --max_number_of_evaluations=1
+#    --eval_scales=0.5\
+#    --eval_scales=0.75\
+#    --eval_scales=1.0\
+#    --eval_scales=1.25\
+#    --eval_scales=1.5\
+#    --eval_scales=1.75\
+#    --add_flipped_images=true
