a1617b5ea49b456793d2a84872d11cb6fb3b18db
long <908865817@qq.com>
2019-01-13 15:46:08 +0800

    v3pluscam0

--- research/deeplab/pyramid13.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sh pyramid_class_aware_train13.sh 2>&1 | tee pyramid_class_aware_train13.log
-
-sh pyramid_class_aware_train13_res50.sh 2>&1 | tee pyramid_class_aware_train13_res50.log
-
-sh pyramid_class_aware_train13_res101.sh 2>&1 | tee pyramid_class_aware_train13_res101.log
--- research/deeplab/v3plusconcat.sh
+++ research/deeplab/v3plusconcat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#sh dual_pyramid_train_v3plusCam.sh 2>&1| tee dual_pyramid_train_v3plusCam_concat256.log
+sh dual_pyramid_train_v3plusCam.sh 2>&1| tee dual_pyramid_train_v3plusCam_concat256.log
 sh dual_pyramid_train_v3plusCam_x65.sh 2>&1| tee dual_pyramid_train_v3plusCam_x65_concat256_2.log
