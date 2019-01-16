434e74d1b4c3a4313dd5d754d31bbd3e95a6da6d
long <908865817@qq.com>
2019-01-16 09:10:06 +0800

    v3pluscam0

--- research/deeplab/dual_pyramid_train_x65_with_boundary.sh
+++ research/deeplab/dual_pyramid_train_x65_with_boundary.sh
@@ -6,7 +6,7 @@ export PYTHONPATH=$PYTHONPATH:$CURRENT_DIR:$CURRENT_DIR/deeplab
 cd ./deeplab
 python dual_pyramid_train_with_boundary.py\
     --logtostderr \
-    --num_clones=4 \
+    --num_clones=2 \
     --train_split="train_aug"\
     --model_variant="xception_65"\
     --atrous_rates=6\
