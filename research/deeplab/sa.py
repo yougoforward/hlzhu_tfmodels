2766c1fbe5985d49327a9f7b7b602be9cd623b1f
long <908865817@qq.com>
2019-01-19 15:37:06 +0800

    sa

--- research/deeplab/sa.py
+++ research/deeplab/sa.py
@@ -72,7 +72,7 @@ def self_attention(features,
           proj_value = tf.reshape(f3, [n, 512, -1])
 
           out = tf.matmul(proj_value, tf.transpose(attention, [0, 2, 1]))
-          out = tf.transpose(tf.reshape(out, [n, c, h, w]), [0, 2, 3, 1])
+          out = tf.reshape(tf.transpose(out, [0,2,1]), [n,h,w,512])
           skip = slim.conv2d(features, 512, 1, scope="skip")
           f5 = slim.conv2d(tf.concat([out, skip],3), 256, 1, scope="sa_proj")
           return f5
