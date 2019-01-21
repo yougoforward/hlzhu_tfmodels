d7f6b174239a24fbfc8840acf9f8934d9c5f6dc1
long <908865817@qq.com>
2019-01-21 14:53:28 +0800

    sa

--- research/deeplab/class_aware_train15_res50.sh
+++ research/deeplab/class_aware_train15_res50.sh
@@ -53,32 +53,32 @@ cd ./deeplab
 #    --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
 #    --max_number_of_evaluations=1
 
-python class_aware_train15.py\
-    --logtostderr\
-    --num_clones=2 \
-    --train_split="train_aug"\
-    --model_variant="resnet_v1_50_beta"\
-    --atrous_rates=6\
-    --atrous_rates=12\
-    --atrous_rates=18\
-    --output_stride=16\
-    --multi_grid=1\
-    --multi_grid=2\
-    --multi_grid=4\
-    --train_crop_size=513\
-    --train_crop_size=513\
-    --train_batch_size=16\
-    --training_number_of_steps=30000\
-    --fine_tune_batch_norm=false\
-    --base_learning_rate=0.001\
-    --weight_decay=0.0001\
-    --aspp_with_batch_norm=true\
-    --aspp_with_separable_conv=false\
-    --decoder_use_separable_conv=false\
-    --dataset="pascal_voc_seg"\
-    --tf_initial_checkpoint="datasets/pascal_voc_seg/exp/class_aware_train15_res50_multigrad_on_trainaug_set/train4/model.ckpt-60000"\
-    --train_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_res50_multigrad_on_trainaug_set/train_finetune4"\
-    --dataset_dir="datasets/pascal_voc_seg/tfrecord"
+#python class_aware_train15.py\
+#    --logtostderr\
+#    --num_clones=2 \
+#    --train_split="train_aug"\
+#    --model_variant="resnet_v1_50_beta"\
+#    --atrous_rates=6\
+#    --atrous_rates=12\
+#    --atrous_rates=18\
+#    --output_stride=16\
+#    --multi_grid=1\
+#    --multi_grid=2\
+#    --multi_grid=4\
+#    --train_crop_size=513\
+#    --train_crop_size=513\
+#    --train_batch_size=16\
+#    --training_number_of_steps=30000\
+#    --fine_tune_batch_norm=false\
+#    --base_learning_rate=0.001\
+#    --weight_decay=0.0001\
+#    --aspp_with_batch_norm=true\
+#    --aspp_with_separable_conv=false\
+#    --decoder_use_separable_conv=false\
+#    --dataset="pascal_voc_seg"\
+#    --tf_initial_checkpoint="datasets/pascal_voc_seg/exp/class_aware_train15_res50_multigrad_on_trainaug_set/train/model.ckpt-60000"\
+#    --train_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_res50_multigrad_on_trainaug_set/train_finetune"\
+#    --dataset_dir="datasets/pascal_voc_seg/tfrecord"
 
 python class_aware_eval15.py\
     --logtostderr\
@@ -97,16 +97,16 @@ python class_aware_eval15.py\
     --aspp_with_separable_conv=false\
     --decoder_use_separable_conv=false\
     --dataset="pascal_voc_seg"\
-    --checkpoint_dir="datasets/pascal_voc_seg/exp/class_aware_train15_res50_multigrad_on_trainaug_set/train_finetune4"\
-    --eval_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_res50_multigrad_on_trainaug_set/eval_finetune4"\
+    --checkpoint_dir="datasets/pascal_voc_seg/exp/class_aware_train15_res50_multigrad_on_trainaug_set/train_finetune"\
+    --eval_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_res50_multigrad_on_trainaug_set/eval_finetune"\
     --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
-    --max_number_of_evaluations=1
-#    --eval_scales=0.5\
-#    --eval_scales=0.75\
-#    --eval_scales=1.0\
-#    --eval_scales=1.25\
-#    --eval_scales=1.5\
-#    --eval_scales=1.75\
+    --max_number_of_evaluations=1\
+    --eval_scales=0.5\
+    --eval_scales=0.75\
+    --eval_scales=1.0\
+    --eval_scales=1.25\
+    --eval_scales=1.5\
+    --eval_scales=1.75
 #    --add_flipped_images=true
 
 
