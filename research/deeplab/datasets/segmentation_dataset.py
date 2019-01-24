5c690e74a109ef3ccf136f3cbc1a31b3158b251a
long <908865817@qq.com>
2019-01-24 20:12:53 +0800

    sa

--- research/deeplab/datasets/segmentation_dataset.py
+++ research/deeplab/datasets/segmentation_dataset.py
@@ -81,6 +81,7 @@ _CITYSCAPES_INFORMATION = DatasetDescriptor(
     splits_to_sizes={
         'train': 2975,
         'val': 500,
+        'test': 1525,
     },
     num_classes=19,
     ignore_label=255,
@@ -92,6 +93,7 @@ _PASCAL_VOC_SEG_INFORMATION = DatasetDescriptor(
         'train_aug': 10582,
         'trainval': 2913,
         'val': 1449,
+        'test': 1456,
     },
     num_classes=21,
     ignore_label=255,
@@ -124,6 +126,80 @@ def get_cityscapes_dataset_name():
 
 
 def get_dataset(dataset_name, split_name, dataset_dir):
+  """Gets an instance of slim Dataset.
+
+  Args:
+    dataset_name: Dataset name.
+    split_name: A train/val Split name.
+    dataset_dir: The directory of the dataset sources.
+
+  Returns:
+    An instance of slim Dataset.
+
+  Raises:
+    ValueError: if the dataset_name or split_name is not recognized.
+  """
+  if dataset_name not in _DATASETS_INFORMATION:
+    raise ValueError('The specified dataset is not supported yet.')
+
+  splits_to_sizes = _DATASETS_INFORMATION[dataset_name].splits_to_sizes
+
+  if split_name not in splits_to_sizes:
+    raise ValueError('data split name %s not recognized' % split_name)
+
+  # Prepare the variables for different datasets.
+  num_classes = _DATASETS_INFORMATION[dataset_name].num_classes
+  ignore_label = _DATASETS_INFORMATION[dataset_name].ignore_label
+
+  file_pattern = _FILE_PATTERN
+  file_pattern = os.path.join(dataset_dir, file_pattern % split_name)
+
+  # Specify how the TF-Examples are decoded.
+  keys_to_features = {
+      'image/encoded': tf.FixedLenFeature(
+          (), tf.string, default_value=''),
+      'image/filename': tf.FixedLenFeature(
+          (), tf.string, default_value=''),
+      'image/format': tf.FixedLenFeature(
+          (), tf.string, default_value='jpeg'),
+      'image/height': tf.FixedLenFeature(
+          (), tf.int64, default_value=0),
+      'image/width': tf.FixedLenFeature(
+          (), tf.int64, default_value=0),
+      'image/segmentation/class/encoded': tf.FixedLenFeature(
+          (), tf.string, default_value=''),
+      'image/segmentation/class/format': tf.FixedLenFeature(
+          (), tf.string, default_value='png'),
+  }
+  items_to_handlers = {
+      'image': tfexample_decoder.Image(
+          image_key='image/encoded',
+          format_key='image/format',
+          channels=3),
+      'image_name': tfexample_decoder.Tensor('image/filename'),
+      'height': tfexample_decoder.Tensor('image/height'),
+      'width': tfexample_decoder.Tensor('image/width'),
+      'labels_class': tfexample_decoder.Image(
+          image_key='image/segmentation/class/encoded',
+          format_key='image/segmentation/class/format',
+          channels=1),
+  }
+
+  decoder = tfexample_decoder.TFExampleDecoder(
+      keys_to_features, items_to_handlers)
+
+  return dataset.Dataset(
+      data_sources=file_pattern,
+      reader=tf.TFRecordReader,
+      decoder=decoder,
+      num_samples=splits_to_sizes[split_name],
+      items_to_descriptions=_ITEMS_TO_DESCRIPTIONS,
+      ignore_label=ignore_label,
+      num_classes=num_classes,
+      name=dataset_name,
+      multi_label=True)
+
+def get_dataset_from_imdir(dataset_name, imdir, split_name, dataset_dir):
   """Gets an instance of slim Dataset.
 
   Args:
