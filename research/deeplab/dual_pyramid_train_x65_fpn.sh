bcd2365617e93a6d42053923488b27f31e22a584
long <908865817@qq.com>
2019-01-25 16:46:28 +0800

    sa

--- research/deeplab/dual_pyramid_train_x65_fpn.sh
+++ research/deeplab/dual_pyramid_train_x65_fpn.sh
@@ -78,6 +78,87 @@ python dual_pyramid_eval_fpn.py\
     --logtostderr\
     --eval_split="val"\
     --model_variant="xception_65"\
+    --atrous_rates=6\
+    --atrous_rates=12\
+    --atrous_rates=18\
+    --output_stride=16\
+    --decoder_output_stride=4\
+    --eval_crop_size=513\
+    --eval_crop_size=513\
+    --aspp_with_batch_norm=true\
+    --aspp_with_separable_conv=false\
+    --decoder_use_separable_conv=false\
+    --dataset="pascal_voc_seg"\
+    --checkpoint_dir="datasets/pascal_voc_seg/exp/dual_pyramid_train_x65_fpn_on_trainaug_set/train_finetune13"\
+    --eval_logdir="datasets/pascal_voc_seg/exp/dual_pyramid_train_x65_fpn_on_trainaug_set/eval_finetune13"\
+    --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
+    --max_number_of_evaluations=1\
+    --eval_scales=0.5\
+    --eval_scales=0.75\
+    --eval_scales=1.0\
+    --eval_scales=1.25\
+    --eval_scales=1.5\
+    --eval_scales=1.75
+#    --add_flipped_images=true
+
+python dual_pyramid_eval_fpn.py\
+    --logtostderr\
+    --eval_split="val"\
+    --model_variant="xception_65"\
+    --atrous_rates=6\
+    --atrous_rates=12\
+    --atrous_rates=18\
+    --output_stride=16\
+    --decoder_output_stride=4\
+    --eval_crop_size=513\
+    --eval_crop_size=513\
+    --aspp_with_batch_norm=true\
+    --aspp_with_separable_conv=false\
+    --decoder_use_separable_conv=false\
+    --dataset="pascal_voc_seg"\
+    --checkpoint_dir="datasets/pascal_voc_seg/exp/dual_pyramid_train_x65_fpn_on_trainaug_set/train_finetune13"\
+    --eval_logdir="datasets/pascal_voc_seg/exp/dual_pyramid_train_x65_fpn_on_trainaug_set/eval_finetune13"\
+    --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
+    --max_number_of_evaluations=1\
+    --eval_scales=0.5\
+    --eval_scales=0.75\
+    --eval_scales=1.0\
+    --eval_scales=1.25\
+    --eval_scales=1.5\
+    --eval_scales=1.75\
+    --add_flipped_images=true
+
+python dual_pyramid_eval_fpn.py\
+    --logtostderr\
+    --eval_split="val"\
+    --model_variant="xception_65"\
+    --atrous_rates=12\
+    --atrous_rates=24\
+    --atrous_rates=36\
+    --output_stride=8\
+    --decoder_output_stride=4\
+    --eval_crop_size=513\
+    --eval_crop_size=513\
+    --aspp_with_batch_norm=true\
+    --aspp_with_separable_conv=false\
+    --decoder_use_separable_conv=false\
+    --dataset="pascal_voc_seg"\
+    --checkpoint_dir="datasets/pascal_voc_seg/exp/dual_pyramid_train_x65_fpn_on_trainaug_set/train_finetune13"\
+    --eval_logdir="datasets/pascal_voc_seg/exp/dual_pyramid_train_x65_fpn_on_trainaug_set/eval_finetune13"\
+    --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
+    --max_number_of_evaluations=1
+#    --eval_scales=0.5\
+#    --eval_scales=0.75\
+#    --eval_scales=1.0\
+#    --eval_scales=1.25\
+#    --eval_scales=1.5\
+#    --eval_scales=1.75\
+#    --add_flipped_images=true
+
+python dual_pyramid_eval_fpn.py\
+    --logtostderr\
+    --eval_split="val"\
+    --model_variant="xception_65"\
     --atrous_rates=12\
     --atrous_rates=24\
     --atrous_rates=36\
