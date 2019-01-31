ac72ae2a2cf2cd1352f39844bf61724610ef2cec
long <908865817@qq.com>
2019-01-31 11:08:37 +0800

    city

--- research/deeplab/cam15_train_cityscapes.sh
+++ research/deeplab/cam15_train_cityscapes.sh
@@ -59,39 +59,39 @@ cd ./deeplab
 #    --add_flipped_images=true
 
 
-python dual_pyramid_train_fpn.py \
-    --logtostderr \
-    --num_clones=4 \
-    --train_split="train"\
-    --model_variant="xception_65"\
-    --atrous_rates=6\
-    --atrous_rates=12\
-    --atrous_rates=18\
-    --output_stride=16\
-    --train_crop_size=769\
-    --train_crop_size=769\
-    --train_batch_size=8\
-    --decoder_output_stride=4 \
-    --training_number_of_steps=90000\
-    --fine_tune_batch_norm=true\
-    --base_learning_rate=0.007\
-    --weight_decay=0.00004\
-    --aspp_with_batch_norm=true\
-    --aspp_with_separable_conv=false\
-    --decoder_use_separable_conv=false\
-    --dataset="cityscapes"\
-    --tf_initial_checkpoint="datasets/cityscapes/exp/dpcan/train/model.ckpt-90000"\
-    --train_logdir="datasets/cityscapes/exp/dpcan/train_finetune"\
-    --dataset_dir="datasets/cityscapes/tfrecord"
-#
+#python dual_pyramid_train_fpn.py \
+#    --logtostderr \
+#    --num_clones=4 \
+#    --train_split="train"\
+#    --model_variant="xception_65"\
+#    --atrous_rates=6\
+#    --atrous_rates=12\
+#    --atrous_rates=18\
+#    --output_stride=16\
+#    --train_crop_size=769\
+#    --train_crop_size=769\
+#    --train_batch_size=8\
+#    --decoder_output_stride=4 \
+#    --training_number_of_steps=90000\
+#    --fine_tune_batch_norm=true\
+#    --base_learning_rate=0.007\
+#    --weight_decay=0.00004\
+#    --aspp_with_batch_norm=true\
+#    --aspp_with_separable_conv=false\
+#    --decoder_use_separable_conv=false\
+#    --dataset="cityscapes"\
+#    --tf_initial_checkpoint="datasets/cityscapes/exp/dpcan/train/model.ckpt-90000"\
+#    --train_logdir="datasets/cityscapes/exp/dpcan/train_finetune"\
+#    --dataset_dir="datasets/cityscapes/tfrecord"
+##
 python dual_pyramid_eval_fpn.py \
     --logtostderr \
     --eval_split="val" \
     --model_variant="xception_65" \
-    --atrous_rates=6 \
     --atrous_rates=12 \
-    --atrous_rates=18 \
-    --output_stride=16 \
+    --atrous_rates=24 \
+    --atrous_rates=36 \
+    --output_stride=8 \
     --decoder_output_stride=4 \
     --eval_crop_size=1025 \
     --eval_crop_size=2049 \
@@ -102,7 +102,14 @@ python dual_pyramid_eval_fpn.py \
     --checkpoint_dir="datasets/cityscapes/exp/dpcan/train_finetune"\
     --eval_logdir="datasets/cityscapes/exp/dpcan/eval_finetune"\
     --dataset_dir="datasets/cityscapes/tfrecord"
-    --max_number_of_iterations=1\
+    --max_number_of_iterations=1
+#    --eval_scales=0.5\
+#    --eval_scales=0.75\
+#    --eval_scales=1.0\
+#    --eval_scales=1.25\
+#    --eval_scales=1.5\
+#    --eval_scales=1.75\
+#    --add_flipped_images=true
 #
 #python train_cityscapes.py \
 #    --logtostderr \
