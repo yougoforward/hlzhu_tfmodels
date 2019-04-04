002fce4e8c1874c4572bfdd865dc7d81e980859d
Hailong Zhu <908865817@qq.com>
2019-04-04 14:54:19 +0400

    ms test

--- research/deeplab/dp_cityscapes.sh
+++ research/deeplab/dp_cityscapes.sh
@@ -152,7 +152,7 @@ python3 dual_pyramid_train_fpn.py \
     --train_batch_size=8\
     --decoder_output_stride=4 \
     --training_number_of_steps=90000\
-    --fine_tune_batch_norm=true\
+    --fine_tune_batch_norm=false\
     --base_learning_rate=0.001\
     --weight_decay=0.00004\
     --aspp_with_batch_norm=true\
