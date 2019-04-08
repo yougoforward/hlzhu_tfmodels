47684fbd76b48ab101c596ebd5b9dd11eb7f525d
Hailong Zhu <908865817@qq.com>
2019-04-08 14:41:30 +0400

    visdpn

--- research/deeplab/vis_dpn.sh
+++ research/deeplab/vis_dpn.sh
@@ -32,36 +32,10 @@ cd ./deeplab
 #  --eval_scales=1.75\
 #  --add_flipped_images=true
 
-#python3 dual_pyramid_eval_fpn.py \
-#    --logtostderr \
-#    --eval_split="val" \
-#    --model_variant="xception_65" \
-#    --atrous_rates=12 \
-#    --atrous_rates=24 \
-#    --atrous_rates=36 \
-#    --output_stride=8 \
-#    --decoder_output_stride=4 \
-#    --eval_crop_size=1025 \
-#    --eval_crop_size=2049 \
-#    --aspp_with_batch_norm=true\
-#    --aspp_with_separable_conv=false\
-#    --decoder_use_separable_conv=false\
-#    --dataset="cityscapes" \
-#    --checkpoint_dir="datasets/cityscapes/exp/dpcan/train3"\
-#    --eval_logdir="datasets/cityscapes/exp/dpcan/eval3_os8_05_175_flip"\
-#    --dataset_dir="datasets/cityscapes/tfrecord"\
-#    --max_number_of_evaluations=1\
-#    --eval_scales=0.5\
-#    --eval_scales=0.75\
-#    --eval_scales=1.0\
-#    --eval_scales=1.25\
-#    --eval_scales=1.5\
-#    --eval_scales=1.75\
-#    --add_flipped_images=true
 
 python3 vis_class_aware2.py \
   --logtostderr \
-  --vis_split="val" \
+  --vis_split="test" \
   --colormap_type="cityscapes" \
   --model_variant="xception_65" \
   --atrous_rates=12 \
@@ -76,7 +50,7 @@ python3 vis_class_aware2.py \
   --decoder_use_separable_conv=false\
   --dataset="cityscapes" \
   --checkpoint_dir="datasets/cityscapes/exp/dpcan/train3"\
-  --vis_logdir="datasets/cityscapes/exp/dpcan/vis_val"\
+  --vis_logdir="datasets/cityscapes/exp/dpcan/vis_test_train"\
   --dataset_dir="datasets/cityscapes/tfrecord"\
   --max_number_of_evaluations=1\
   --eval_scales=0.5\
@@ -86,3 +60,32 @@ python3 vis_class_aware2.py \
   --eval_scales=1.5\
   --eval_scales=1.75\
   --add_flipped_images=true
+
+
+#python3 vis_class_aware2.py \
+#  --logtostderr \
+#  --vis_split="val" \
+#  --colormap_type="cityscapes" \
+#  --model_variant="xception_65" \
+#  --atrous_rates=12 \
+#  --atrous_rates=24 \
+#  --atrous_rates=36 \
+#  --output_stride=8 \
+#  --decoder_output_stride=4\
+#  --vis_crop_size=1025 \
+#  --vis_crop_size=2049 \
+#  --aspp_with_batch_norm=true\
+#  --aspp_with_separable_conv=false\
+#  --decoder_use_separable_conv=false\
+#  --dataset="cityscapes" \
+#  --checkpoint_dir="datasets/cityscapes/exp/dpcan/train3"\
+#  --vis_logdir="datasets/cityscapes/exp/dpcan/vis_val"\
+#  --dataset_dir="datasets/cityscapes/tfrecord"\
+#  --max_number_of_evaluations=1\
+#  --eval_scales=0.5\
+#  --eval_scales=0.75\
+#  --eval_scales=1.0\
+#  --eval_scales=1.25\
+#  --eval_scales=1.5\
+#  --eval_scales=1.75\
+#  --add_flipped_images=true
