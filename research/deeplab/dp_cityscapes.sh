890b4834274a7668ec61bb5c8cc3668c5e521dac
hlzhu <908865817@qq.com>
2019-04-29 21:02:44 +0400

    visdpn

--- research/deeplab/dp_cityscapes.sh
+++ research/deeplab/dp_cityscapes.sh
@@ -74,7 +74,61 @@ python3 dual_pyramid_eval_fpn.py \
     --eval_logdir="datasets/cityscapes/exp/dpcan/eval4"\
     --dataset_dir="datasets/cityscapes/tfrecord"\
     --max_number_of_evaluations=1
-#
+
+python3 dual_pyramid_eval_fpn.py \
+    --logtostderr \
+    --eval_split="val" \
+    --model_variant="xception_65" \
+    --atrous_rates=12 \
+    --atrous_rates=24 \
+    --atrous_rates=36 \
+    --output_stride=8 \
+    --decoder_output_stride=4 \
+    --eval_crop_size=1025 \
+    --eval_crop_size=2049 \
+    --aspp_with_batch_norm=true\
+    --aspp_with_separable_conv=false\
+    --decoder_use_separable_conv=false\
+    --dataset="cityscapes" \
+    --checkpoint_dir="datasets/cityscapes/exp/dpcan/train4"\
+    --eval_logdir="datasets/cityscapes/exp/dpcan/eval4_os8"\
+    --dataset_dir="datasets/cityscapes/tfrecord"\
+    --max_number_of_evaluations=1
+#    --eval_scales=0.5\
+#    --eval_scales=0.75\
+#    --eval_scales=1.0\
+#    --eval_scales=1.25\
+#    --eval_scales=1.5\
+#    --eval_scales=1.75\
+#    --add_flipped_images=true
+
+python3 dual_pyramid_eval_fpn.py \
+    --logtostderr \
+    --eval_split="val" \
+    --model_variant="xception_65" \
+    --atrous_rates=12 \
+    --atrous_rates=24 \
+    --atrous_rates=36 \
+    --output_stride=8 \
+    --decoder_output_stride=4 \
+    --eval_crop_size=1025 \
+    --eval_crop_size=2049 \
+    --aspp_with_batch_norm=true\
+    --aspp_with_separable_conv=false\
+    --decoder_use_separable_conv=false\
+    --dataset="cityscapes" \
+    --checkpoint_dir="datasets/cityscapes/exp/dpcan/train4"\
+    --eval_logdir="datasets/cityscapes/exp/dpcan/eval4_os8_175"\
+    --dataset_dir="datasets/cityscapes/tfrecord"\
+    --max_number_of_evaluations=1\
+    --eval_scales=0.5\
+    --eval_scales=0.75\
+    --eval_scales=1.0\
+    --eval_scales=1.25\
+    --eval_scales=1.5\
+    --eval_scales=1.75\
+    --add_flipped_images=true
+
 #python3 dual_pyramid_eval_fpn.py \
 #    --logtostderr \
 #    --eval_split="val" \
