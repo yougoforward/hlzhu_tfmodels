63a571ed709394dc2aa79d84d72aacad5ea7711e
hlzhu <908865817@qq.com>
2019-07-02 10:04:53 +0400

    ade

--- research/deeplab/dp_coco_01.sh
+++ research/deeplab/dp_coco_01.sh
@@ -4,10 +4,57 @@ CURRENT_DIR=$(pwd)
 export PYTHONPATH=$PYTHONPATH:$CURRENT_DIR:$CURRENT_DIR/slim
 export PYTHONPATH=$PYTHONPATH:$CURRENT_DIR:$CURRENT_DIR/deeplab
 cd ./deeplab
+#python dual_pyramid_train_fpn.py\
+#    --logtostderr \
+#    --num_clones=8 \
+#    --train_split="trainval"\
+#    --model_variant="xception_65"\
+#    --atrous_rates=6\
+#    --atrous_rates=12\
+#    --atrous_rates=18\
+#    --output_stride=16\
+#    --train_crop_size=513\
+#    --train_crop_size=513\
+#    --train_batch_size=32\
+#    --decoder_output_stride=4\
+#    --training_number_of_steps=90000\
+#    --fine_tune_batch_norm=true\
+#    --base_learning_rate=0.02\
+#    --weight_decay=0.00004\
+#    --aspp_with_batch_norm=true\
+#    --aspp_with_separable_conv=false\
+#    --decoder_use_separable_conv=false\
+#    --dataset="coco_voc"\
+#    --tf_initial_checkpoint="datasets/pascal_voc_seg/init_models/xception/model.ckpt"\
+#    --train_logdir="datasets/coco/exp/dpx65_cocovoc1000_train_set/train"\
+#    --dataset_dir="datasets/coco/tfrecord"
+##
+#python dual_pyramid_eval_fpn.py\
+#    --logtostderr\
+#    --eval_split="val"\
+#    --model_variant="xception_65"\
+#    --atrous_rates=6 \
+#    --atrous_rates=12 \
+#    --atrous_rates=18 \
+#    --output_stride=16 \
+#    --decoder_output_stride=4 \
+#    --eval_crop_size=513 \
+#    --eval_crop_size=513 \
+#    --min_resize_value=513 \
+#    --max_resize_value=513 \
+#    --aspp_with_batch_norm=true \
+#    --aspp_with_separable_conv=false \
+#    --decoder_use_separable_conv=false \
+#    --dataset="pascal_voc_seg" \
+#    --checkpoint_dir="datasets/coco/exp/dpx65_cocovoc1000_train_set/train" \
+#    --eval_logdir="datasets/coco/exp/dpx65_cocovoc1000_val_set/eval" \
+#    --dataset_dir="datasets/coco/tfrecord" \
+#    --max_number_of_evaluations=1
+
 python dual_pyramid_train_fpn.py\
     --logtostderr \
-    --num_clones=8 \
-    --train_split="trainval"\
+    --num_clones=4 \
+    --train_split="train_aug"\
     --model_variant="xception_65"\
     --atrous_rates=6\
     --atrous_rates=12\
@@ -15,86 +62,39 @@ python dual_pyramid_train_fpn.py\
     --output_stride=16\
     --train_crop_size=513\
     --train_crop_size=513\
-    --train_batch_size=32\
+    --train_batch_size=16\
     --decoder_output_stride=4\
-    --training_number_of_steps=90000\
+    --training_number_of_steps=60000\
     --fine_tune_batch_norm=true\
-    --base_learning_rate=0.02\
+    --base_learning_rate=0.001\
     --weight_decay=0.00004\
     --aspp_with_batch_norm=true\
     --aspp_with_separable_conv=false\
     --decoder_use_separable_conv=false\
-    --dataset="coco_voc"\
-    --tf_initial_checkpoint="datasets/pascal_voc_seg/init_models/xception/model.ckpt"\
-    --train_logdir="datasets/coco/exp/dpx65_cocovoc1000_train_set/train"\
-    --dataset_dir="datasets/coco/tfrecord"
+    --dataset="pascal_voc_seg"\
+    --tf_initial_checkpoint="datasets/coco/exp/dpx65_cocovoc1000_train_set/train/model-90000.ckpt"\
+    --train_logdir="datasets/coco/exp/dpx65_voc_trainaug_set/train"\
+    --dataset_dir="datasets/pascal_voc_seg/tfrecord"
 #
 python dual_pyramid_eval_fpn.py\
     --logtostderr\
     --eval_split="val"\
     --model_variant="xception_65"\
-    --atrous_rates=6 \
-    --atrous_rates=12 \
-    --atrous_rates=18 \
-    --output_stride=16 \
-    --decoder_output_stride=4 \
-    --eval_crop_size=513 \
-    --eval_crop_size=513 \
-    --min_resize_value=513 \
-    --max_resize_value=513 \
-    --aspp_with_batch_norm=true \
-    --aspp_with_separable_conv=false \
-    --decoder_use_separable_conv=false \
-    --dataset="pascal_voc_seg" \
-    --checkpoint_dir="datasets/coco/exp/dpx65_cocovoc1000_train_set/train" \
-    --eval_logdir="datasets/coco/exp/dpx65_cocovoc1000_val_set/eval" \
-    --dataset_dir="datasets/coco/tfrecord" \
+    --atrous_rates=6\
+    --atrous_rates=12\
+    --atrous_rates=18\
+    --output_stride=16\
+    --decoder_output_stride=4\
+    --eval_crop_size=513\
+    --eval_crop_size=513\
+    --aspp_with_batch_norm=true\
+    --aspp_with_separable_conv=false\
+    --decoder_use_separable_conv=false\
+    --dataset="pascal_voc_seg"\
+    --checkpoint_dir="datasets/coco/exp/dpx65_voc_trainaug_set/train"\
+    --eval_logdir="datasets/coco/exp/dpx65_voc_trainaug_set/val"\
+    --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
     --max_number_of_evaluations=1
-
-#python dual_pyramid_train_fpn.py\
-#    --logtostderr \
-#    --num_clones=4 \
-#    --train_split="train_aug"\
-#    --model_variant="xception_65"\
-#    --atrous_rates=6\
-#    --atrous_rates=12\
-#    --atrous_rates=18\
-#    --output_stride=16\
-#    --train_crop_size=513\
-#    --train_crop_size=513\
-#    --train_batch_size=16\
-#    --decoder_output_stride=4\
-#    --training_number_of_steps=60000\
-#    --fine_tune_batch_norm=true\
-#    --base_learning_rate=0.001\
-#    --weight_decay=0.00004\
-#    --aspp_with_batch_norm=true\
-#    --aspp_with_separable_conv=false\
-#    --decoder_use_separable_conv=false\
-#    --dataset="pascal_voc_seg"\
-#    --tf_initial_checkpoint="datasets/pascal_voc_seg/init_models/xception/model.ckpt"\
-#    --train_logdir="datasets/coco/exp/dpx65_voc_trainaug_set/train"\
-#    --dataset_dir="datasets/pascal_voc_seg/tfrecord"
-##
-#python dual_pyramid_eval_fpn.py\
-#    --logtostderr\
-#    --eval_split="val"\
-#    --model_variant="xception_65"\
-#    --atrous_rates=6\
-#    --atrous_rates=12\
-#    --atrous_rates=18\
-#    --output_stride=16\
-#    --decoder_output_stride=4\
-#    --eval_crop_size=513\
-#    --eval_crop_size=513\
-#    --aspp_with_batch_norm=true\
-#    --aspp_with_separable_conv=false\
-#    --decoder_use_separable_conv=false\
-#    --dataset="pascal_voc_seg"\
-#    --checkpoint_dir="datasets/coco/exp/dpx65_voc_trainaug_set/train"\
-#    --eval_logdir="datasets/coco/exp/dpx65_voc_trainaug_set/val"\
-#    --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
-#    --max_number_of_evaluations=1
 
 ##
 #python dual_pyramid_train_fpn.py\
