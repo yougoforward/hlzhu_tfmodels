c04ef34d757867926ac3aa4a1b275dbe1397d763
hlzhu <908865817@qq.com>
2019-07-07 14:42:48 +0400

    vis

--- research/deeplab/vis_class_aware2.py
+++ research/deeplab/vis_class_aware2.py
@@ -249,7 +249,9 @@ def main(unused_argv):
           eval_scales=FLAGS.eval_scales,
           add_flipped_images=FLAGS.add_flipped_images)
     predictions = predictions[common.OUTPUT_TYPE]
-
+    # predictions = predictions['attention1']
+    # predictions = predictions['attention2']
+    # predictions = predictions['attention3']
     if FLAGS.min_resize_value and FLAGS.max_resize_value:
       # Only support batch_size = 1, since we assume the dimensions of original
       # image after tf.squeeze is [height, width, 3].
