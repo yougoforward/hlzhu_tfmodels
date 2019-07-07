19c63739ec3059f4978abf3714dde592c079f4ec
hlzhu <908865817@qq.com>
2019-07-07 14:55:53 +0400

    vis

--- research/deeplab/vis_class_aware.sh
+++ research/deeplab/vis_class_aware.sh
@@ -29,50 +29,50 @@ cd ./deeplab
 #  --eval_scales=1.5\
 #  --eval_scales=1.75\
 #  --add_flipped_images=true
+#
+#python vis_class_aware2.py \
+#  --logtostderr \
+#  --vis_split="val" \
+#  --model_variant="xception_65" \
+#  --atrous_rates=12 \
+#  --atrous_rates=24 \
+#  --atrous_rates=36 \
+#  --output_stride=8 \
+#  --vis_crop_size=513 \
+#  --vis_crop_size=513 \
+#  --aspp_with_batch_norm=true\
+#  --aspp_with_separable_conv=false\
+#  --decoder_use_separable_conv=false\
+#  --checkpoint_dir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/train_finetune2"\
+#  --vis_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/vis_train_finetune2"\
+#  --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
+#  --max_number_of_iterations=1 \
+#  --eval_scales=0.5\
+#  --eval_scales=0.75\
+#  --eval_scales=1.0\
+#  --eval_scales=1.25\
+#  --eval_scales=1.5\
+#  --eval_scales=1.75\
+#  --add_flipped_images=true
 
 python vis_class_aware2.py \
   --logtostderr \
   --vis_split="val" \
   --model_variant="xception_65" \
+  --atrous_rates=6 \
   --atrous_rates=12 \
-  --atrous_rates=24 \
-  --atrous_rates=36 \
-  --output_stride=8 \
+  --atrous_rates=18 \
+  --output_stride=16 \
   --vis_crop_size=513 \
   --vis_crop_size=513 \
+  --vis_batch_size=1 \
   --aspp_with_batch_norm=true\
   --aspp_with_separable_conv=false\
   --decoder_use_separable_conv=false\
   --checkpoint_dir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/train_finetune2"\
-  --vis_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/vis_train_finetune2"\
+  --vis_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/vis_train_finetune2_os16"\
   --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
-  --max_number_of_iterations=1 \
-  --eval_scales=0.5\
-  --eval_scales=0.75\
-  --eval_scales=1.0\
-  --eval_scales=1.25\
-  --eval_scales=1.5\
-  --eval_scales=1.75\
-  --add_flipped_images=true
-
-#python vis_class_aware2.py \
-#  --logtostderr \
-#  --vis_split="val" \
-#  --model_variant="xception_65" \
-#  --atrous_rates=6 \
-#  --atrous_rates=12 \
-#  --atrous_rates=18 \
-#  --output_stride=16 \
-#  --vis_crop_size=513 \
-#  --vis_crop_size=513 \
-#  --vis_batch_size=1 \
-#  --aspp_with_batch_norm=true\
-#  --aspp_with_separable_conv=false\
-#  --decoder_use_separable_conv=false\
-#  --checkpoint_dir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/train_finetune2"\
-#  --vis_logdir="datasets/pascal_voc_seg/exp/class_aware_train15_on_trainaug_set/vis_train_finetune2_os16"\
-#  --dataset_dir="datasets/pascal_voc_seg/tfrecord"\
-#  --max_number_of_iterations=1
+  --max_number_of_iterations=1
 #  --eval_scales=0.5\
 #  --eval_scales=0.75\
 #  --eval_scales=1.0\
